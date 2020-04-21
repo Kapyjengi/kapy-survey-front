843429f632dcc7de74d60862ec2ca085159a9e83
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-04-21 13:41:11 +0300

    Update Answer.js

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -9,37 +9,52 @@ import React, { useEffect, useState } from 'react';
 
 
     saadaan propsina surveyID, haetaan kyselyn kysymys 1
+
+    /surveys/{surveyId}/{questionNumber}
+     surveys/{surveyId}/questions
+ /surveys/{surveyId}/submitanswers
+
 */
 
 export default function Answer(props){
     const [answer, setAnswer] = useState('');
-    const [question, setQuestion] = useState('');
+    const [questions, setQuestions] = useState([]);
     const [questionNumber, setQuestionNumber] =useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState('');
 
     useEffect(() => {
-        getQuestion();
+        // getQuestions();
+        testData(); // TEST
+        displayNextQuestion();
     }, [])
 
-    const getQuestion = () => {
-        fetch('https://kapysurvey-back.herokuapp.com/' + props.surveyId
-        + '/' + questionNumber
-        )//lisätään tähän endpoint, kysymys 1, 2, 3 jne...
+    // TEST
+    const testData = () => {
+        setQuestions(["Oliko hyvaa", "Miltä tuntuu", "Katsoitko peiliin"]);
+    }
+ 
+    const getQuestions = () => {
+        fetch('https://kapysurvey-back.herokuapp.com/surveys/' + props.surveyId + '/questions'
+        )
             .then(response => response.json())
-            .then(data => setQuestion(data.questions)) // SYNTAKSI
+            .then(data => setQuestions(data.questions)) // TODO
+            .then(_ => displayNextQuestion())
             .catch(err => console.error(err))
     }
 
+    const displayNextQuestion = () => {
+        setCurrentQuestion(questions[questionNumber]);
+        setQuestionNumber(questionNumber + 1);
+    }
+
     const submitAnswer= () => {
-        fetch ('https://kapysurvey-back.herokuapp.com/surveys/', // MIKÄ URL
+        fetch ('https://kapysurvey-back.herokuapp.com/surveys/' + props.surveyId +"/submitanswers" ,
         {
         method: 'POST', 
         headers: {'Content-Type': 'application/json' }
         ,
         body: JSON.stringify(answer)
     })
-        .then(_ => {
-            getQuestion()} // HAETAAN SEURAAVA Kysymys
-        )
         .catch(err => console.error(err))
     
     }
@@ -48,13 +63,14 @@ export default function Answer(props){
         setAnswer({...answer,[event.target.name]:event.target.value});
       };
       // {question}
-      // 
-    return (
+      // <button onClick={submitAnswer}>Send</button>
+    
+      return (
         <div>
-            <h1> KYSYMYS </h1> 
+            <h1> {currentQuestion} </h1> 
             <div>
             <textarea type="text" name="answer" value={answer.value} onChange={inputChanged} />
-            <button onClick={submitAnswer}>Send</button>
+            <button onClick={displayNextQuestion}>Next</button>
           </div>
         </div>
     );
