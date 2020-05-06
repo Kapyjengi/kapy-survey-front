1fbfe0dc4c887d8ebd397b2298585fa4639537a9
Paivi Lampinen <bet983@myy.haaga-helia.fi>
2020-05-06 13:54:20 +0300

    fixing answerreport funtionality

--- src/components/AnswerlistTreedata.js
+++ src/components/AnswerlistTreedata.js
@@ -18,33 +18,17 @@ export default function ShowAnswerlistTreeData(props) {
             .catch(err => console.error(err))
     }
     console.log(list)
-    let koko=[];
+
     let vas=[];
-    let x=0;
-    let xx=[];
     for (const iterator of list) {
       //console.log(iterator.answers)
       for (let i = 0; i < iterator.answers.length; i++) { 
         vas.push({id:iterator.questionId, questionText: iterator.questionText,answer:iterator.answers[i].answerText,qid:iterator.questionId})
-        //koko.push({questionId: iterator.questionId,questionText: iterator.questionText,answer:iterator.answers,qid:iterator.questionId}) 
+        
       }
     }
     //console.log(vas)
-    let aaa = []
-    for (let i = 0; i < vas.length; i++) {
-      let iidee=vas[i].id
-      if (!xx.includes(iidee)) {
-        aaa=vas[i].answer
-        //console.log(vas[i].answer)
-        xx.push(vas[i].id)
-      } else {
-        koko.push({question:vas[i].questionText,id:vas[i].id,answer:vas[i].answer, answer:aaa})
-      }
-    }
-    /*    console.log(list)
-    console.log(koko) */
-    //console.log(aaa)
-
+    
     const tableIcons = { 
       FirstPage: FirstPage,
       LastPage: LastPage,
@@ -59,14 +43,13 @@ export default function ShowAnswerlistTreeData(props) {
     return (
         <div>
             <Typography 
-            gutterBottom variant='h6'
-            component='h6'
+            gutterBottom variant='h6' component='h6'
             style={{ marginTop: 50 }}>Käytä hakukenttää haluamasi kysymyksen vastausten hakuun.
             </Typography>
             <MaterialTable
                 style={{ marginTop: 20, marginLeft: 10, marginRight: 10 }}
                 title={props.surveyName + " - " + props.surveyDescription}
-                data={koko,vas}
+                data={vas}
                   columns={[
                     /* { title: 'questionId', field: 'questionId' }, */
                     { title: 'Question', field: 'questionText' },
